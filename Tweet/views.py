8548295f03d603472a7d69f12fd82e7547d2ea64
MahZ21 <Mahletz2711@gmail.com>
2023-04-27 14:52:58 +0300

    new clone

--- Tweet/views.py
+++ Tweet/views.py
@@ -4,6 +4,7 @@ from .models import Profile, Tweet
 from .forms import TweetForm, SignUpForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import User
 from django import forms
 
 
@@ -103,5 +104,20 @@ def register_user(request):
     return render(request, "register.html", {'form': form})
 
 def update_user(request):
-    return render(request, "update_user.html", {})
+    if request.user.is_authenticated:
+       current_user = User.objects.get(id=request.user.id) 
+       form = SignUpForm(request.POST or None, instance= current_user)
+       if form.is_valid():
+           form.save() 
+           login(request, current_user)
+           messages.success(request, ("Your Profile Has Been Updated!"))
+           return redirect('home')
+          
+           
+       return render(request, "update_user.html", {'form': form})    
+    else: 
+        messages.success(request, ("You must be Logged In!"))
+        return redirect('home')
+   
+    
     
